2e319b331019c6947fd01a0e1dff1f891830f51b
khludy <khludy3@gmail.com>
2023-05-11 23:51:10 -0700

    web

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,44 +1,39 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-"""
-
+""" a script that starts a Flask web application """
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
+def hello_hbnb():
+    """ displays Hello HBNB! """
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a given string"""
-    return ("HBNB")
+    """ displays HBNB """
+    return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def cText(text):
-    """display C followed by the value of the text variable"""
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """ displays c + text """
+    return "C %s" % text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pythonText(text="is cool"):
-    """display Python followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text):
+    """ displays Python + text """
+    return "Python %s" % text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def isNumber(n):
-    """display “n is a number” only if n is an integer"""
-    if isinstance(n, int):
-        return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ displays: n is a number """
+    return "%d is a number" % n
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-
+""" a script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage, State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """Render state_list html page to display States created"""
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
+def remove_session(exception):
+    """ After each request, it removes the current SQLAlchemy Session """
     storage.close()
 
 
-if __name__ == "__main__":
+@app.route('/states_list', strict_slashes=False)
+def render_states():
+    """ displays all states """
+    States = storage.all(State).values()
+    return render_template("7-states_list.html", States=States)
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
